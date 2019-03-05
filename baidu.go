a5d2987a79ee7fbb167e41c77c5acd104b7bf83e
vcico <cgjcgs@gmail.com>
2019-03-05 18:35:27 +0800

    #

---
+++ baidu.go
@@ -0,0 +1,12 @@
+package main
+
+/*
+破解翻译接口  https://www.jianshu.com/p/5f3177943b91
+https://www.codetd.com/article/2164560
+https://www.jianshu.com/p/2c333f7ae1c2
+*/
+
+/**
+ * 单词 百度翻译 ： 音标、中文、短语、例句
+ */
+
